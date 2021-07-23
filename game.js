6395a345c0eaf5a33ce3c0936ac894be3059765b
johnnion1 <jonathan.burak1@gmail.com>
2021-07-23 22:49:26 +0200

    Deleted first approach and added new function rockps()

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-function computerPay() {
+function computerSelect() {
 
     let computerChoice = Math.random()*100;
     if (computerChoice >= 66) {
@@ -17,39 +17,10 @@ function computerPay() {
 }
 
 function playerSelect() {
-    let playerSelection = prompt("Choose R, P or S!");
+    let playerSelection = prompt("Choose Rock, Paper or Scissors!");
     return playerSelection.toLowerCase() 
 }
 
-function parseWinner() {
-   let computerPlay= computerPay();
-   // console.log(computerPlay);
-   let playerPlay = playerSelect();
- //  console.log(playerPlay);
-
-    if (computerPlay === "Rock") {
-        if (playerPlay === "rock") {
-            console.log("It's a draw, Rock and Rock.");
-        } else if (playerPlay === "paper") {
-            console.log("You win! Paper beats Rock!");
-        } else if (playerPlay === "scissors") {
-            console.log("You loose! Rock beats Scissors!");
-        }
-    } else if (computerPlay === "Paper") {
-        if (playerPlay === "rock") {
-            console.log("You loose! Paper beats Rock!");
-        } else if (playerPlay === "paper") {
-            console.log("It's a draw, Paper and Paper.");
-        } else if (playerPlay === "scissors") {
-            console.log("You win! Scissors beats Paper!");
-        }
-    } else if (computerPlay === "Scissors") {
-        if (playerPlay === "rock") {
-            console.log("You win! Rock beats Scissors!");
-        } else if (playerPlay === "paper") {
-            console.log("You loose! Scissors beats Paper!");
-        } else if (playerPlay === "scissors") {
-            console.log("It's a draw, scissors and scissors.");
-        }
-    }
-    }
+function rockps() {
+    
+}
