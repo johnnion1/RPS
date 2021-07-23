59adc3a6b6d97c4d4f1a5baedf4069fe03b9e291
johnnion1 <jonathan.burak1@gmail.com>
2021-07-23 22:59:56 +0200

    Reused old function by replacing console.log with return and adjusted the arguments passed into the function when called

--- game.js
+++ game.js
@@ -21,6 +21,31 @@ function playerSelect() {
     return playerSelection.toLowerCase() 
 }
 
-function rockps() {
-    
-}
+function rockps(playerselection, computerselection) {
+
+         if (computerselection === "Rock") {
+             if (playerselection === "rock") {
+                 return("It's a draw, Rock and Rock.");
+             } else if (playerselection === "paper") {
+                return("You win! Paper beats Rock!");
+             } else if (playerselection === "scissors") {
+                return("You loose! Rock beats Scissors!");
+             }
+         } else if (computerselection === "Paper") {
+             if (playerselection === "rock") {
+                return("You loose! Paper beats Rock!");
+             } else if (playerselection === "paper") {
+                return("It's a draw, Paper and Paper.");
+             } else if (playerselection === "scissors") {
+                return("You win! Scissors beats Paper!");
+             }
+         } else if (computerselection === "Scissors") {
+             if (playerselection === "rock") {
+                return("You win! Rock beats Scissors!");
+             } else if (playerselection === "paper") {
+                return("You loose! Scissors beats Paper!");
+             } else if (playerselection === "scissors") {
+                return("It's a draw, scissors and scissors.");
+             }
+         }
+         }
