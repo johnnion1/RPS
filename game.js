c98f9bc32b45d0cd13bb47aad586cb937d6dae6f
johnnion1 <jonathan.burak1@gmail.com>
2021-07-26 21:52:58 +0200

    Changed rockps return to log the results instead

--- game.js
+++ game.js
@@ -16,36 +16,57 @@ function computerSelect() {
     }
 }
 
-function playerSelect() {
+function playerSelect() {  // PLAYER SRELECT!!
     let playerSelection = prompt("Choose Rock, Paper or Scissors!");
     return playerSelection.toLowerCase() 
 }
 
+
+//loop the rockps(), add two counters = 0, if return contains substring win: repeat until one counter reaches 5, gratulate winner
+function game() { 
+
+    for(; playerScore < 5 || computerScore < 5;) {
+        rockps(playerSelect(), computerSelect());
+        console.log(playerScore, computerScore);
+    
+    }
+
+}
 function rockps(playerselection, computerselection) {
 
-         if (computerselection === "Rock") {
-             if (playerselection === "rock") {
-                 return("It's a draw, Rock and Rock.");
-             } else if (playerselection === "paper") {
-                return("You win! Paper beats Rock!");
-             } else if (playerselection === "scissors") {
-                return("You loose! Rock beats Scissors!");
-             }
-         } else if (computerselection === "Paper") {
-             if (playerselection === "rock") {
-                return("You loose! Paper beats Rock!");
-             } else if (playerselection === "paper") {
-                return("It's a draw, Paper and Paper.");
-             } else if (playerselection === "scissors") {
-                return("You win! Scissors beats Paper!");
-             }
-         } else if (computerselection === "Scissors") {
-             if (playerselection === "rock") {
-                return("You win! Rock beats Scissors!");
-             } else if (playerselection === "paper") {
-                return("You loose! Scissors beats Paper!");
-             } else if (playerselection === "scissors") {
-                return("It's a draw, scissors and scissors.");
-             }
-         }
-         }
+    let playerScore = 0;
+    let computerScore = 0;
+
+    if (computerselection === "Rock") {
+        if (playerselection === "rock") {
+            console.log("It's a draw, Rock and Rock.");
+        } else if (playerselection === "paper") {
+            playerScore++;
+           console.log("You win! Paper beats Rock!");
+        } else if (playerselection === "scissors") {
+            computerScore++;
+           console.log("You loose! Rock beats Scissors!");
+        }
+    } else if (computerselection === "Paper") {
+        if (playerselection === "rock") {
+            computerScore++;
+           console.log("You loose! Paper beats Rock!");
+        } else if (playerselection === "paper") {
+           console.log("It's a draw, Paper and Paper.");
+        } else if (playerselection === "scissors") {
+            playerScore++;
+           console.log("You win! Scissors beats Paper!");
+        }
+    } else if (computerselection === "Scissors") {
+        if (playerselection === "rock") {
+            playerScore++;
+           console.log("You win! Rock beats Scissors!");
+        } else if (playerselection === "paper") {
+            computerScore++;
+           console.log("You loose! Scissors beats Paper!");
+        } else if (playerselection === "scissors") {
+           console.log("It's a draw, scissors and scissors.");
+        }
+    }
+  //  if (playerScore > 3) {alert("WINNER!!!!!!");}
+    }
