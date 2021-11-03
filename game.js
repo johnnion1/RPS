9fad3ffa8004502344040a5ad51f925816dcf6f7
johnnion1 <jonathan.burak1@gmail.com>
2021-11-03 11:03:13 +0100

    Scrubbed old code

--- game.js
+++ game.js
@@ -2,37 +2,21 @@ function computerSelect() {
 
     let computerChoice = Math.random()*100;
     if (computerChoice >= 66) {
-
-     //  console.log("rOCk");
         return "Rock";
     } else if (computerChoice >= 33) {
-
-      // console.log("pAPer");
         return "Paper";
     } else if (computerChoice >= 0) {
-
-     //  console.log("scISSsors");
         return "Scissors";
     }
 }
 
-function playerSelect() { 
-    let playerSelection = document.querySelector('button.id');
-    if (playerSelection === 1) {
-        return "rock";
-    }
-    else if (playerSelection === 2) {
-        return "paper";
-    }
-    else if (playerSelection === 3) {
-        return "scissors";
-    } 
-}
+
 
 function rockps(playerselection, computerselection) {
 
     let playerScore = 0;
     let computerScore = 0;
+  
 
     if (computerselection === "Rock") {
         if (playerselection === "rock") {
@@ -75,3 +59,15 @@ function rockps(playerselection, computerselection) {
         }
     }
     }
+
+    
+     /*  let getselection = document.querySelector('button.id');
+    if (getselection === 1) {
+        playerselection = "rock";
+    }
+    else if (getselection === 2) {
+        return "paper";
+    }
+    else if (getselection === 3) {
+        return "scissors";
+    }  */
